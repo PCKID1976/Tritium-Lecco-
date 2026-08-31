a7bdebb827f2fe634df91e8ba72548d0a2bdfa30
PCKID1976 <m.ciapparelli@touchrevolution.it>
2026-08-31 12:41:33 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="presenze-agonistica-sync-v2";
+const CORE=["./","./index.html","./manifest.webmanifest"];
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{if(e.request.mode==="navigate"){e.respondWith(fetch(e.request,{cache:"no-store"}).catch(()=>caches.match("./")));return}e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
